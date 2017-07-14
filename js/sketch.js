0967d66be317b18dd5a3d7f625550c60f9eb1a06
Jayanth Prathipati <jayanth@vt.edu>
2017-07-14 11:48:36 -0400

    added comments

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
 var osc, fft;
 
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var loc = 0;
 var datasize = 0;
@@ -49,7 +49,7 @@ var dragging = false;
 var dataReceived = false;
 
 var lastMonth = [];
-var lastThreeMonths = []; 
+var lastThreeMonths = [];
 var lastSixMonths = [];
 var lastOneYear = [];
 var lastFiveYears = [];
@@ -58,6 +58,8 @@ var lastFiveYears = [];
 var controlPress = false;
 var plusPress = false;
 var minusPress = false;
+
+//TODO: move API key out of repository 
 var quandlQ = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?api_key=iz12PA5nC-YLyESare9X&qopts.columns=open,high,low,close,volume,date";
 var ticker = "AAPL";
 var tickerCompany = "Apple";
@@ -66,84 +68,71 @@ var toDate = new Date();
 var addtl = "&ticker=" + ticker + "&date.gte=" + toJSONLocal(fromDate) + "&date.lte=" + toJSONLocal(toDate);
 
 var query = quandlQ + addtl;
-// var query = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?api_key=iz12PA5nC-YLyESare9X&qopts.columns=open,high,low,close,volume&ticker=AAPL&date.gte=2016-07-10&date.lte=2017-01-13"; 
+
 
 
 var data = [];
 
-function resetLoc() {
-    console.log(loc, data.length);
-    if(loc > data.length-1) {
-        loc = 0;
-    }
-}
 
-$(document).ready(function () {
+//functions that are required to run pre-startup 
+$(document).ready(function() {
 
-    $( "#onemonth" ).mousedown(function() {
-      timerange = "onemonth";
-      data = setData();
-      deselectAll();
-      $(this).addClass( 'buttonSelected' );
-      //console.log("onemonth");
+    $("#onemonth").mousedown(function() {
+        timerange = "onemonth";
+        data = setData();
+        deselectAll();
+        $(this).addClass('buttonSelected');
     });
 
-    $( "#threemonths" ).mousedown(function() {
-      timerange = "threemonths";
-      data = setData();
-      deselectAll();
-      $(this).addClass( 'buttonSelected' );
-      //console.log("threemonths");
+    $("#threemonths").mousedown(function() {
+        timerange = "threemonths";
+        data = setData();
+        deselectAll();
+        $(this).addClass('buttonSelected');
     });
 
-    $( "#sixmonths" ).mousedown(function() {
-      timerange = "sixmonths";
-      data = setData();
-      deselectAll();
-      $(this).addClass( 'buttonSelected' );
-      //console.log("sixmonths");
+    $("#sixmonths").mousedown(function() {
+        timerange = "sixmonths";
+        data = setData();
+        deselectAll();
+        $(this).addClass('buttonSelected');
     });
 
-    $( "#oneyear" ).mousedown(function() {
-      timerange = "oneyear";
-      data = setData();
-      deselectAll();
-      $(this).addClass( 'buttonSelected' );
-      //console.log("oneyear");
+    $("#oneyear").mousedown(function() {
+        timerange = "oneyear";
+        data = setData();
+        deselectAll();
+        $(this).addClass('buttonSelected');
     });
 
-    $( "#fiveyears" ).mousedown(function() {
-      timerange = "fiveyears";
-      data = setData();
-      deselectAll();
-      $(this).addClass( 'buttonSelected' );
-      //console.log("fiveyears");
+    $("#fiveyears").mousedown(function() {
+        timerange = "fiveyears";
+        data = setData();
+        deselectAll();
+        $(this).addClass('buttonSelected');
     });
 
-    $('#input').keyup(function(){
+    $('#input').keyup(function() {
 
-        if($(this).val().length !=0) {
-            $('#submit').attr('disabled', false);  
+        if ($(this).val().length != 0) {
+            $('#submit').attr('disabled', false);
         } else {
-            $('#submit').attr('disabled',true);
+            $('#submit').attr('disabled', true);
         }
 
     })
-
-
-
-    
 });
 
 function deselectAll() {
-    $( "#onemonth" ).removeClass( 'buttonSelected' );
-    $( "#threemonths" ).removeClass( 'buttonSelected' );
-    $( "#sixmonths" ).removeClass( 'buttonSelected' );
-    $( "#oneyear" ).removeClass( 'buttonSelected' );
-    $( "#fiveyears" ).removeClass( 'buttonSelected' );
+    $("#onemonth").removeClass('buttonSelected');
+    $("#threemonths").removeClass('buttonSelected');
+    $("#sixmonths").removeClass('buttonSelected');
+    $("#oneyear").removeClass('buttonSelected');
+    $("#fiveyears").removeClass('buttonSelected');
 }
 
 
+//functions to get date string from application 
 function toLocal(date) {
     var local = new Date(date);
     local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
@@ -156,6 +145,7 @@ function toJSONLocal(date) {
     return local.toJSON().slice(0, 10);
 }
 
+//day class to get ohlc 
 class Day {
 
     constructor(date, open, high, low, close, volume, sethigh, setlow) {
@@ -170,45 +160,44 @@ class Day {
     }
 }
 
+
+
 function setData() {
 
 
-    var dataset; 
-    //change item here 
-    if(timerange == "onemonth"){
+    var dataset;
+    //TODO: add notification so users know that the location changed
+    if (timerange == "onemonth") {
         dataset = lastMonth;
-        if(loc > lastMonth.length) {
-            loc = 0; 
+        if (loc > lastMonth.length) {
+            loc = 0;
         }
-    } else if(timerange == "threemonths"){
+    } else if (timerange == "threemonths") {
         dataset = lastThreeMonths;
-        if(loc > lastThreeMonths.length) {
-            loc = 0; 
+        if (loc > lastThreeMonths.length) {
+            loc = 0;
         }
-    } else if(timerange == "sixmonths"){
+    } else if (timerange == "sixmonths") {
         dataset = lastSixMonths;
-        if(loc > lastSixMonths.length) {
-            loc = 0; 
+        if (loc > lastSixMonths.length) {
+            loc = 0;
         }
-    } else if(timerange == "oneyear"){
+    } else if (timerange == "oneyear") {
         dataset = lastOneYear;
-        if(loc > lastOneYear.length) {
-            loc = 0; 
+        if (loc > lastOneYear.length) {
+            loc = 0;
         }
-    } else if(timerange == "fiveyears"){
+    } else if (timerange == "fiveyears") {
         dataset = lastFiveYears;
-        if(loc > lastFiveYears.length) {
-            loc = 0; 
+        if (loc > lastFiveYears.length) {
+            loc = 0;
         }
     }
-    resetLoc();
     return dataset;
 }
 
 
 function getData() {
-    console.log("getData");
-    
     var fromDate = new Date();
     fromDate.setFullYear(new Date().getFullYear() - 5);
     var toDate = new Date();
@@ -216,23 +205,19 @@ function getData() {
     var query = quandlQ + addtl;
 
     $.getJSON(query).done(function(d) {
-      afterData(d);
+        afterData(d);
     })
-
-    // .then(function(d) {
-    //   console.log("data! " + data);
-    //   afterData(d);
-    // });
 }
 
-function afterData( thedata ) {
+function afterData(thedata) {
 
     lastFiveYears = [];
-    console.log("after load");
     var sethigh = -1;
     var setlow = Number.MAX_SAFE_INTEGER;
 
 
+
+    //get low and get high? 
     thedata['datatable']['data'].forEach(function(element) {
 
         if (element[1] > sethigh) {
@@ -247,47 +232,48 @@ function afterData( thedata ) {
     thedata['datatable']['data'].forEach(function(element) {
         var d = new Date(element[5]);
         d.setDate(d.getDate() + 1);
-        var newDate = ""+months[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear();
+        var newDate = "" + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
         var today = new Day(newDate, element[0], element[1], element[2], element[3], element[4], sethigh, setlow, d);
         lastFiveYears.push(today);
     });
 
 
-    lastMonth = []; 
-    var lastMonthDate = new Date(); 
+    lastMonth = [];
+    var lastMonthDate = new Date();
     lastMonthDate.setMonth(new Date().getMonth() - 1);
 
-    lastThreeMonths = []; 
-    var lastThreeMonthsDate = new Date(); 
+    lastThreeMonths = [];
+    var lastThreeMonthsDate = new Date();
     lastThreeMonthsDate.setMonth(new Date().getMonth() - 3);
 
-    lastSixMonths = []; 
-    var lastSixMonthsDate = new Date(); 
+    lastSixMonths = [];
+    var lastSixMonthsDate = new Date();
     lastSixMonthsDate.setMonth(new Date().getMonth() - 6);
 
-    lastOneYear = []; 
-    var lastYearDate = new Date(); 
+    lastOneYear = [];
+    var lastYearDate = new Date();
     lastYearDate.setFullYear(new Date().getFullYear() - 1);
 
-    var i = lastFiveYears.length - 1; 
-    var end = i - 365; 
+    var i = lastFiveYears.length - 1;
+    var end = i - 365;
 
-    for(i; i >= end; i--) {
-        
-        var item = lastFiveYears[i]; 
+    for (i; i >= end; i--) {
+
+        var item = lastFiveYears[i];
         var thedate = new Date(item.date);
 
-        if(thedate > lastMonthDate) {
+
+        if (thedate > lastMonthDate) {
             lastMonth.push(item);
         }
-        if(thedate > lastThreeMonthsDate) {
-            lastThreeMonths.push(item); 
+        if (thedate > lastThreeMonthsDate) {
+            lastThreeMonths.push(item);
         }
-        if(thedate > lastSixMonthsDate) {
-            lastSixMonths.push(item); 
+        if (thedate > lastSixMonthsDate) {
+            lastSixMonths.push(item);
         }
-        if(thedate > lastYearDate) {
-            lastOneYear.push(item); 
+        if (thedate > lastYearDate) {
+            lastOneYear.push(item);
         }
     }
 
@@ -299,15 +285,15 @@ function updateRate() {
 }
 
 function preload() {
-  table = loadTable("assets/tickers.csv", "csv", "header");
-  getData();
+    table = loadTable("assets/tickers.csv", "csv", "header");
+    getData();
 }
 
 function setup() {
 
-    $('#submit').attr('disabled',true);
-    $("#tickerName").text("Company: " +tickerCompany);
-    $( "#oneyear" ).addClass( 'buttonSelected' );
+    $('#submit').attr('disabled', true);
+    $("#tickerName").text("Company: " + tickerCompany);
+    $("#oneyear").addClass('buttonSelected');
 
     createCanvas(windowWidth, canvasHeight);
 
@@ -338,14 +324,12 @@ function playNote(note, duration) {
 function draw() {
 
 
+    //what is going on here? 
     prevLoc = loc;
 
-    if(prevLoc != loc) {
+    if (prevLoc != loc) {
         newLoc = true;
     }
-
-    // console.log("month playing: "+monthPlaying);
-    // console.log("details playing: "+detailsPlaying);
 
     background(255);
 
@@ -361,24 +345,24 @@ function draw() {
 
 }
 
-function isInside(){
-    if(mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height){
+function isInside() {
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
         return true;
     } else {
         return false;
     }
 }
 
-function mousePressed(){
-    if(isInside()) {
-        loc = Math.floor( map(mouseX, 0, width, 0, data.length-1) );
+function mousePressed() {
+    if (isInside()) {
+        loc = Math.floor(map(mouseX, 0, width, 0, data.length - 1));
         playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
     }
 }
 
-function mouseDragged(){
-    if(isInside()) {
-        loc = Math.floor( map(mouseX, 0, width, 0, data.length-1) );
+function mouseDragged() {
+    if (isInside()) {
+        loc = Math.floor(map(mouseX, 0, width, 0, data.length - 1));
         playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
     }
 }
@@ -389,13 +373,13 @@ function changeTicker() {
     try {
         tickerCompany = row.getString("Description");
 
-        $("#tickerName").text("Company: " +tickerCompany);
+        $("#tickerName").text("Company: " + tickerCompany);
         $(".tickerfield").val("");
         dataReceived = false;
-    
+
         getData();
         data = setData();
-    
+
         if (data != undefined && data[0] != undefined) {
             playChangeSound();
             resetLoc();
@@ -404,21 +388,18 @@ function changeTicker() {
             setTimeout(function() { playChangeSound(); }, 100);
         }
         $("#submit").blur();
-        $('#submit').attr('disabled',true);
-    }
-
-
-    catch(err){
+        $('#submit').attr('disabled', true);
+    } catch (err) {
         textToSpeech.speak(ticker + "is not a valid ticker name");
     }
-    
+
     if (data != undefined && data[0] != undefined) {
         playChangeSound();
         resetLoc();
     } else {
         setTimeout(function() { playChangeSound(); }, 100);
     }
-    
+
 }
 
 function playValue() {
@@ -443,21 +424,21 @@ function playValue() {
             var low = " Low: " + data[loc].low;
 
 
-            if(data[loc].open > data[loc].close){
+            if (data[loc].open > data[loc].close) {
 
-                textToSpeech.speak("Downward Trend, " + high + open + close + low); 
+                textToSpeech.speak("Downward Trend, " + high + open + close + low);
 
-            } else if(data[loc].close > data[loc].open){
+            } else if (data[loc].close > data[loc].open) {
 
-                textToSpeech.speak("Upward Trend, " + low + open + close + high); 
+                textToSpeech.speak("Upward Trend, " + low + open + close + high);
 
             } else {
 
                 textToSpeech.speak("Neutral Trend " + high + open + low);
-                textToSpeech.onEnd(function(){ detailsPlaying = false;});
+                textToSpeech.onEnd(function() { detailsPlaying = false; });
 
             }
-            
+
 
         }
 
@@ -488,7 +469,7 @@ function changeRate() {
 
     if (key == '-' && rate > 0.3) {
         rate -= 0.2;
-        
+
         updateRate();
 
         buttonDown = false;
@@ -498,22 +479,22 @@ function changeRate() {
 function playChangeSound() {
     var rangeString;
 
-    if(timerange == "onemonth"){
+    if (timerange == "onemonth") {
         rangeString = "One month prior to today";
-    } else if(timerange == "threemonths"){
+    } else if (timerange == "threemonths") {
         rangeString = "Three months prior to today";
-    } else if(timerange == "sixmonths"){
+    } else if (timerange == "sixmonths") {
         rangeString = "Six months prior to today";
-    } else if(timerange == "oneyear"){
+    } else if (timerange == "oneyear") {
         rangeString = "One year prior to today";
-    } else if(timerange == "fiveyears"){
+    } else if (timerange == "fiveyears") {
         rangeString = "Five years prior to today";
     }
 
-    var pt = data[data.length-1].close - data[data.length-1].open;
-    var pcnt = pt/data[data.length-1].open *100;
-    
-    textToSpeech.speak("Changed to" + tickerCompany+". Current price: " + data[data.length-1].close+". Percent Change. "+  pcnt +" Point Change. "+  pt +" Date Range "+rangeString+""); 
+    var pt = data[data.length - 1].close - data[data.length - 1].open;
+    var pcnt = pt / data[data.length - 1].open * 100;
+
+    textToSpeech.speak("Changed to" + tickerCompany + ". Current price: " + data[data.length - 1].close + ". Percent Change. " + pcnt + " Point Change. " + pt + " Date Range " + rangeString + "");
 }
 
 
@@ -532,7 +513,7 @@ function checkLeftRight() {
 
     if (key == 'g' && loc > 0) {
 
-        if(detailsPlaying){
+        if (detailsPlaying) {
             stopSpeech();
         }
 
@@ -540,45 +521,45 @@ function checkLeftRight() {
 
         playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
 
-        if(loc == 0){
-            textToSpeech.speak("Beginning"); 
+        if (loc == 0) {
+            textToSpeech.speak("Beginning");
         }
 
     } else if (key == 'h' && loc < data.length - 1) {
 
-        if(detailsPlaying){
+        if (detailsPlaying) {
             stopSpeech();
         }
-        
+
         loc++;
 
         playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
 
-        if(loc == data.length-1){
-            textToSpeech.speak("End"); 
-        } 
+        if (loc == data.length - 1) {
+            textToSpeech.speak("End");
+        }
 
     }
 
 
-    
+
 }
 
 function checkMonth() {
 
-    
-    if(loc - 1 >= 0){
+
+    if (loc - 1 >= 0) {
         var currentDate = new Date(data[loc].date);
-        var previousDate = new Date(data[loc-1].date);
+        var previousDate = new Date(data[loc - 1].date);
 
 
-        if(currentDate.getMonth() != previousDate.getMonth()){
+        if (currentDate.getMonth() != previousDate.getMonth()) {
             monthPlaying = true;
-            textToSpeech.speak(months[currentDate.getMonth()]); 
-            textToSpeech.onEnd(function() { monthPlaying = false;})
-        } 
+            textToSpeech.speak(months[currentDate.getMonth()]);
+            textToSpeech.onEnd(function() { monthPlaying = false; })
+        }
     }
-    
+
 }
 
 
@@ -586,7 +567,7 @@ function checkBegEnd() {
 
     if (key == '.') {
 
-        if(detailsPlaying){
+        if (detailsPlaying) {
             stopSpeech();
         }
 
@@ -597,7 +578,7 @@ function checkBegEnd() {
 
     } else if (key == ',') {
 
-        if(detailsPlaying){
+        if (detailsPlaying) {
             stopSpeech();
         }
 
@@ -615,7 +596,7 @@ function drawVis() {
     var padding = 100;
 
     var newLow = window.height - padding;
-    var newHigh = 0 + padding; 
+    var newHigh = 0 + padding;
 
 
     if (data != undefined && data[0] != undefined) {
@@ -629,9 +610,9 @@ function drawVis() {
 
             if (i != 0 && i != data.length) {
 
-                var xPos = map(i, 0, data.length-1, 0, width);
+                var xPos = map(i, 0, data.length - 1, 0, width);
 
-                var lastxPos = map(i - 1, 0, data.length-1, 0, width);
+                var lastxPos = map(i - 1, 0, data.length - 1, 0, width);
 
                 stroke(0);
 
@@ -654,7 +635,7 @@ function drawVis() {
         }
 
         stroke(255, 0, 0);
-        var curMapped = map(loc, 0, data.length-1, 0, width);
+        var curMapped = map(loc, 0, data.length - 1, 0, width);
         line(curMapped, 0, curMapped, canvasHeight);
         fill(255, 0, 0);
         ellipse(curMapped, map(data[loc].close, data[loc].setlow, data[loc].sethigh, newLow, newHigh), 5, 5);
@@ -662,7 +643,6 @@ function drawVis() {
 
 }
 
-function windowResized() { 
-    resizeCanvas(windowWidth, canvasHeight); 
+function windowResized() {
+    resizeCanvas(windowWidth, canvasHeight);
 }
-
