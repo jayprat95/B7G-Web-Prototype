4879c43c3c57c55cb4770ef651c5d298ce714740
Clare Marie Carroll <clare.carroll@bodylabs.com>
2017-07-18 10:12:25 -0400

    Merge branch 'master' into fixes-before-studies

--- js/sketch.js
+++ js/sketch.js
@@ -14,6 +14,9 @@ var lowclose;
 var highmap = 70;
 var lowmap = 50;
 
+var sethigh = -1;
+var setlow = Number.MAX_SAFE_INTEGER;
+
 var durationLeng = 100;
 
 var toneDuration = 1000;
@@ -26,11 +29,15 @@ var table;
 
 var timerange = "oneyear";
 
+var monthPlaying;
+
+var stopTime = 0;
+
 var osc;
 
-var buttonDown = false;
+var currentGraph = 1;
 
-var monthPlaying = false;
+var buttonDown = false;
 
 var textToSpeech = new p5.Speech();
 
@@ -48,12 +55,18 @@ var dragging = false;
 
 var dataReceived = false;
 
+var keyLength = 0;
+
 var lastMonth = [];
 var lastThreeMonths = [];
 var lastSixMonths = [];
 var lastOneYear = [];
 var lastFiveYears = [];
 
+var localHigh;
+var localLow;
+var localMagHigh;
+var localMagLow;
 
 var controlPress = false;
 var plusPress = false;
@@ -120,7 +133,24 @@ $(document).ready(function() {
             $('#submit').attr('disabled', true);
         }
 
-    })
+    });
+
+    $("#graphView").mousedown(function(){
+
+        var on = "Turn On Study";
+        var off = "Turn Off Study"
+
+        if($( this ).attr( "value" ) ==  on){
+            $(this).attr("aria-label",off);
+            $(this).attr("value",off);
+            currentGraph = 2;
+        } else if($( this ).attr( "value" ) ==  off){
+            $(this).attr("aria-label",on);
+            $(this).attr("value",on);
+            currentGraph = 1;
+        }
+        
+    });
 });
 
 function deselectAll() {
@@ -148,15 +178,18 @@ function toJSONLocal(date) {
 //day class to get ohlc 
 class Day {
 
-    constructor(date, open, high, low, close, volume, sethigh, setlow) {
+
+    constructor(dateStr, open, high, low, close, volume, date, sma50, magnitude, overOrUnder) {
+        this.dateStr = dateStr;
         this.date = date;
         this.open = open;
         this.high = high;
         this.low = low;
         this.close = close;
         this.volume = volume;
-        this.sethigh = sethigh;
-        this.setlow = setlow;
+        this.sma50 = sma50; 
+        this.magnitude = magnitude; 
+        this.overOrUnder = overOrUnder; 
     }
 }
 
@@ -193,6 +226,12 @@ function setData() {
             loc = 0;
         }
     }
+
+    localHigh = getHighLow(dataset)[0];
+    localLow = getHighLow(dataset)[1];
+    localMagHigh = getHighLow(dataset)[2];
+    localMagLow = getHighLow(dataset)[3];
+
     return dataset;
 }
 
@@ -200,6 +239,8 @@ function setData() {
 function getData() {
     var fromDate = new Date();
     fromDate.setFullYear(new Date().getFullYear() - 5);
+    //get earlier dates for moving averages 
+    fromDate.setDate(fromDate.getDate() - 200);
     var toDate = new Date();
     var addtl = "&ticker=" + ticker + "&date.gte=" + toJSONLocal(fromDate) + "&date.lte=" + toJSONLocal(toDate);
     var query = quandlQ + addtl;
@@ -212,12 +253,25 @@ function getData() {
 function afterData(thedata) {
 
     lastFiveYears = [];
-    var sethigh = -1;
-    var setlow = Number.MAX_SAFE_INTEGER;
+    sethigh = -1;
+    setlow = Number.MAX_SAFE_INTEGER;
+    var fromDate = new Date();
+    fromDate.setFullYear(new Date().getFullYear() - 5);
 
 
+    var unprocessedData = thedata['datatable']['data']; 
 
-    //get low and get high? 
+    //find the right date 
+    var index = 0; 
+    for (var i = 0; i < unprocessedData.length; i++) {
+        var currDate = new Date(unprocessedData[i][5]); 
+        if(currDate > fromDate) {
+            index = i; 
+            break; 
+        }
+    }
+
+
     thedata['datatable']['data'].forEach(function(element) {
 
         if (element[1] > sethigh) {
@@ -226,16 +280,47 @@ function afterData(thedata) {
         if (element[2] < setlow) {
             setlow = element[2];
         }
-    });
+    }); 
 
-
-    thedata['datatable']['data'].forEach(function(element) {
+    thedata['datatable']['data'].forEach(function(element, i) {
         var d = new Date(element[5]);
         d.setDate(d.getDate() + 1);
+
+        //base case make it 0
+        var sma50 = 0; 
+        if(i >= index) {
+            if(i >= 50) {
+                for(var j = (i - 50); j < i; j++) {
+                    //fix this 
+                    sma50 += thedata['datatable']['data'][j][3]; 
+                }
+                sma50 = sma50/50; 
+            }
+        }
+        
+        var magnitude = Math.abs(sma50 - element[3]); 
+        magnitude = parseFloat((magnitude).toFixed(5)); 
+
+        //if it intersects then it's 0
+        var direction = 0; 
+
+        if((sma50 - element[3]) > 0) {
+            direction = 1; 
+        }
+        else if((sma50 - element[3]) < 0) {
+            direction = -1; 
+        }
+        sma50 = parseFloat((sma50).toFixed(4)); 
+
+        
+
+
         var newDate = "" + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
-        var today = new Day(newDate, element[0], element[1], element[2], element[3], element[4], sethigh, setlow, d);
+        var today = new Day(newDate, element[0], element[1], element[2], element[3], element[4], d, sma50, magnitude, direction);
         lastFiveYears.push(today);
     });
+
+    
 
 
     lastMonth = [];
@@ -283,6 +368,12 @@ function afterData(thedata) {
     lastOneYear.reverse();
 
     data = setData();
+
+    if (data != undefined && data[0] != undefined) {
+        setTickerDetails();
+    } else {
+        setTimeout(function() { setTickerDetails(); }, 100);
+    }
 }
 
 function updateRate() {
@@ -292,6 +383,7 @@ function updateRate() {
 function preload() {
     table = loadTable("assets/tickers.csv", "csv", "header");
     getData();
+
 }
 
 function setup() {
@@ -299,6 +391,16 @@ function setup() {
     $('#submit').attr('disabled', true);
     $("#tickerName").text("Company: " + tickerCompany);
     $("#oneyear").addClass('buttonSelected');
+
+    var graphName;
+
+    if(currentGraph == 1 ) {
+        graphName = "Closing price view";
+    } else if (currentGraph == 2) {
+        graphName = "Study view";
+    }
+
+    $("#currentGraph").text(graphName);
 
     createCanvas(windowWidth, canvasHeight);
 
@@ -316,7 +418,29 @@ function setup() {
 
 function playNote(note, duration) {
     osc.freq(midiToFreq(note));
-    //osc.amp(1);
+    osc.amp(1);
+    osc.fade(1, 0.1);
+
+    if (duration) {
+        setTimeout(function() {
+            osc.fade(0, 0.2);
+        }, duration - 50);
+    }
+}
+
+function playMag(note, duration, abovebelow) {
+
+    if(abovebelow == 1) {
+        osc.amp(1);
+        osc.setType('sawtooth');
+    } else if(abovebelow == -1) {
+        osc.amp(1);
+        osc.setType('triangle');
+    }
+    
+    console.log(note);
+
+    osc.freq(midiToFreq(note));
     osc.fade(1, 0.1);
 
     if (duration) {
@@ -329,23 +453,42 @@ function playNote(note, duration) {
 function draw() {
 
 
-    prevLoc = loc;
-
-    if (prevLoc != loc) {
-        newLoc = true;
+    if(keyIsPressed === false) {
+        stopTime = 1;
+    } else {
+        stopTime = 0;
     }
+
 
     background(255);
 
-    drawVis();
+    if(currentGraph == 1) {
+        drawVisGraphA();
+        console.log("graph A");
+    }
+
+    if(currentGraph == 2) {
+        drawVisGraphB();
+        console.log("graph B");
+    }
+    
 
     if (buttonDown) {
+        
         checkLeftRight();
         playValue();
         changeRate();
         checkBegEnd();
         checkMonth();
     }
+
+    if (prevLoc != loc) {
+        newLoc = true;
+    } else {
+        newLoc = false;
+    }
+
+    prevLoc = loc;
 
 }
 
@@ -360,14 +503,14 @@ function isInside() {
 function mousePressed() {
     if (isInside()) {
         loc = Math.floor(map(mouseX, 0, width, 0, data.length - 1));
-        playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
+        playNote(map(data[loc].close, setlow, sethigh, lowmap, highmap), durationLeng);
     }
 }
 
 function mouseDragged() {
     if (isInside()) {
         loc = Math.floor(map(mouseX, 0, width, 0, data.length - 1));
-        playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
+        playNote(map(data[loc].close, setlow, sethigh, lowmap, highmap), durationLeng);
     }
 }
 
@@ -377,29 +520,15 @@ function changeTicker() {
     try {
         tickerCompany = row.getString("Description");
 
-        $("#tickerName").text("Company: " + tickerCompany);
+        $("#tickerName").text("Company: " + tickerCompany+", ");
         $(".tickerfield").val("");
         dataReceived = false;
 
         getData();
-        data = setData();
-
-        if (data != undefined && data[0] != undefined) {
-            playChangeSound();
-            console.log("changesound!");
-        } else {
-            setTimeout(function() { playChangeSound(); }, 100);
-        }
         $("#submit").blur();
         $('#submit').attr('disabled', true);
     } catch (err) {
         textToSpeech.speak(ticker + "is not a valid ticker name");
-    }
-
-    if (data != undefined && data[0] != undefined) {
-        playChangeSound();
-    } else {
-        setTimeout(function() { playChangeSound(); }, 100);
     }
 
 }
@@ -413,7 +542,7 @@ function playValue() {
             detailsPlaying = false;
             buttonDown = false;
 
-        } else if (detailsPlaying == false) {
+        } else if (detailsPlaying == false) {   
 
             detailsPlaying = true;
             buttonDown = false;
@@ -478,7 +607,7 @@ function changeRate() {
     }
 }
 
-function playChangeSound() {
+function setTickerDetails() {
     var rangeString;
 
     if (timerange == "onemonth") {
@@ -493,21 +622,28 @@ function playChangeSound() {
         rangeString = "Five years prior to today";
     }
 
-    var pt = data[data.length - 1].close - data[data.length - 1].open;
-    var pcnt = pt / data[data.length - 1].open * 100;
+    var pt = (data[data.length - 1].close - data[data.length - 1].open).toFixed(4);
+    var pcnt = (pt / data[data.length - 1].open * 100).toFixed(4);
 
-    textToSpeech.speak("Changed to" + tickerCompany + ". Current price: " + data[data.length - 1].close + ". Percent Change. " + pcnt + " Point Change. " + pt + " Date Range " + rangeString + "");
+    //textToSpeech.speak("Changed to" + tickerCompany + ". Current price: " + data[data.length - 1].close + ". Percent Change. " + pcnt + " Point Change. " + pt + " Date Range " + rangeString + "");
+
+    $("#current-price").text("Current Price: " + data[data.length - 1].close);
+    $("#percent-change").text("Percent Change: " + pcnt);
+    $("#point-change").text("Point Change: " + pt);
+    $("#date-range").text("Date Range: " + rangeString);
 }
 
 
 function keyPressed() {
 
     buttonDown = true;
+
 }
 
 function keyReleased() {
 
     buttonDown = false;
+    keyLength = 0;
 }
 
 
@@ -519,9 +655,18 @@ function checkLeftRight() {
             stopSpeech();
         }
 
-        loc--;
+        if(keyLength == 0 || keyLength > 10) {
+            loc--;
 
-        playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
+            if(currentGraph == 1){
+                playNote(map(data[loc].close, localLow, localHigh, lowmap, highmap), durationLeng);
+            } else if (currentGraph == 2) {
+                playMag(map(data[loc].magnitude, localMagLow, localMagHigh, lowmap, highmap), durationLeng, data[loc].overOrUnder);
+            }
+            
+        }
+
+        keyLength++;
 
         if (loc == 0) {
             textToSpeech.speak("Beginning");
@@ -532,10 +677,19 @@ function checkLeftRight() {
         if (detailsPlaying) {
             stopSpeech();
         }
+        
 
-        loc++;
+        if(keyLength == 0 || keyLength > 10) {
+            loc++;
 
-        playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
+            if(currentGraph == 1){
+                playNote(map(data[loc].close, localLow, localHigh, lowmap, highmap), durationLeng);
+            } else if (currentGraph == 2) {
+                playMag(map(data[loc].magnitude, localMagLow, localMagHigh, lowmap, highmap), durationLeng, data[loc].overOrUnder);
+            }
+        }
+
+        keyLength++;
 
         if (loc == data.length - 1) {
             textToSpeech.speak("End");
@@ -543,22 +697,17 @@ function checkLeftRight() {
 
     }
 
-
-
 }
 
 function checkMonth() {
-
 
     if (loc - 1 >= 0) {
         var currentDate = new Date(data[loc].date);
         var previousDate = new Date(data[loc - 1].date);
 
-
         if (currentDate.getMonth() != previousDate.getMonth()) {
             monthPlaying = true;
-
-            console.log(currentDate.getMonth());
+        
 
             if(currentDate.getMonth() == 0) {
                 textToSpeech.speak(currentDate.getFullYear()+" "+months[currentDate.getMonth()]);
@@ -583,9 +732,13 @@ function checkBegEnd() {
             stopSpeech();
         }
 
+
         loc = data.length - 1;
 
-        playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
+        if(stopTime == 0){
+            playNote(map(data[loc].close, setlow, sethigh, lowmap, highmap), durationLeng);
+        }
+        
 
 
     } else if (key == ',') {
@@ -596,13 +749,46 @@ function checkBegEnd() {
 
         loc = 0;
 
-        playNote(map(data[loc].close, data[loc].setlow, data[loc].sethigh, lowmap, highmap), durationLeng);
-
+        if(stopTime == 0){
+            playNote(map(data[loc].close, setlow, sethigh, lowmap, highmap), durationLeng);
+        }
     }
 }
 
+function getHighLow(myArray) {
 
-function drawVis() {
+    var highlow = [];
+
+    var low = myArray[0].close;
+    var high = myArray[0].close;
+
+    var maglow = myArray[0].magnitude;
+    var maghigh = myArray[0].magnitude;
+
+    for (var i = 0; i < myArray.length; i++) {
+        if (myArray[i].close > high) {
+            high = myArray[i].close;
+        } else if (myArray[i].close < low) {
+            low = myArray[i].close;
+        }
+
+        if (myArray[i].magnitude > maghigh) {
+            maghigh = myArray[i].magnitude;
+        } else if (myArray[i].magnitude < maglow) {
+            maglow = myArray[i].magnitude;
+        }
+    }
+    highlow.push(high);
+    highlow.push(low);
+    highlow.push(maghigh);
+    highlow.push(maglow);
+
+    return highlow;
+
+}
+
+
+function drawVisGraphA() {
 
 
     var padding = 100;
@@ -613,9 +799,7 @@ function drawVis() {
 
     if (data != undefined && data[0] != undefined) {
 
-        var lastY = map(data[0].close, data[0].setlow, data[0].sethigh, newLow, newHigh);
-        // var lastUB = dataset.getRow(0).arr[5] * multiplier + shift;
-        // var lastLB = dataset.getRow(0).arr[6] * multiplier + shift;
+        var lastY = map(data[0].close, localLow,localHigh, newLow, newHigh);
 
         for (var i in data) {
 
@@ -627,30 +811,87 @@ function drawVis() {
                 var lastxPos = map(i - 1, 0, data.length - 1, 0, width);
 
                 stroke(0);
+                strokeWeight(1);
 
-                line(lastxPos, lastY, xPos, map(data[i].close, data[i].setlow, data[i].sethigh, newLow, newHigh));
-
-
-
-                // stroke(216, 11, 207);
-                // line(lastxPos, lastUB, xPos, dataset.getRow(i).arr[5] * multiplier + shift);
-
-                // stroke(47, 229, 37);
-                // line(lastxPos, lastLB, xPos, dataset.getRow(i).arr[6] * multiplier + shift);
-
+                line(lastxPos, lastY, xPos, map(data[i].close, localLow,localHigh, newLow, newHigh));
             }
 
-            lastY = map(data[i].close, data[0].setlow, data[0].sethigh, newLow, newHigh);
-            // lastUB = dataset.getRow(i).arr[5] * multiplier + shift;
-            // lastLB = dataset.getRow(i).arr[6] * multiplier + shift;
+            lastY = map(data[i].close, localLow,localHigh, newLow, newHigh);
+        }
 
+        stroke(255, 0, 0);
+        strokeWeight(1);
+        var curMapped = map(loc, 0, data.length - 1, 0, width);
+        line(curMapped, 0, curMapped, canvasHeight);
+        fill(255, 0, 0);
+        ellipse(curMapped, map(data[loc].close, localLow,localHigh, newLow, newHigh), 5, 5);
+    }
+
+}
+
+function drawVisGraphB() {
+
+    strokeWeight(1);
+
+    var padding = 100;
+
+    var newLow = window.height - padding;
+    var newHigh = 0 + padding;
+
+
+    if (data != undefined && data[0] != undefined) {
+
+        var lastY = map(data[0].close, localLow,localHigh, newLow, newHigh);
+        var lastS = map(data[0].sma50, localLow,localHigh, newLow, newHigh);
+
+        for (var i in data) {
+
+            if (i != 0 && i != data.length) {
+
+                var xPos = map(i, 0, data.length - 1, 0, width);
+
+                var lastxPos = map(i - 1, 0, data.length - 1, 0, width);
+
+                
+
+                if(data[i].overOrUnder == 1) {
+                    strokeWeight(0.5);
+                    fill(216,25,75);
+                    stroke(216,25,75);
+                    quad(lastxPos, lastS, lastxPos, lastY, xPos, map(data[i].close, localLow,localHigh, newLow, newHigh), xPos, map(data[i].sma50, localLow,localHigh, newLow, newHigh));
+                   
+                } else if(data[i].overOrUnder == -1) {
+                    strokeWeight(0.5);
+                    fill(60,173,23);
+                    stroke(60,173,23);
+                    quad(lastxPos, lastS, lastxPos, lastY, xPos, map(data[i].close, localLow,localHigh, newLow, newHigh), xPos, map(data[i].sma50, localLow,localHigh, newLow, newHigh));
+                }
+
+
+                if(data[i].sma50 != 0 || data[i-1].sma50 != 0) {
+                    strokeWeight(1);
+                    stroke(0,67,234);
+                    line(lastxPos, lastS, xPos, map(data[i].sma50, localLow,localHigh, newLow, newHigh));
+                }
+
+                stroke(1);
+                line(lastxPos, lastY, xPos, map(data[i].close, localLow,localHigh, newLow, newHigh));
+                
+            }
+
+            lastY = map(data[i].close, localLow,localHigh, newLow, newHigh);
+            lastS = map(data[i].sma50, localLow,localHigh, newLow, newHigh);
         }
 
         stroke(255, 0, 0);
         var curMapped = map(loc, 0, data.length - 1, 0, width);
         line(curMapped, 0, curMapped, canvasHeight);
         fill(255, 0, 0);
-        ellipse(curMapped, map(data[loc].close, data[loc].setlow, data[loc].sethigh, newLow, newHigh), 5, 5);
+        // ellipse(curMapped, map(data[loc].close, localLow,localHigh, newLow, newHigh), 5, 5);
+        strokeWeight(2);
+        stroke(0);
+        line(curMapped, map(data[loc].close, localLow,localHigh, newLow, newHigh), curMapped, map(data[loc].sma50, localLow,localHigh, newLow, newHigh));
+
     }
 
 }
