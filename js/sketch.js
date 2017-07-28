aa42abc048c1e95dfcc4087a239af69f091016c1
Clare Marie Carroll <clare.carroll@bodylabs.com>
2017-07-28 09:54:48 -0400

    Fixed blur after enter

--- js/sketch.js
+++ js/sketch.js
@@ -125,6 +125,7 @@ $(document).ready(function() {
     $('#input').keydown(function(e) {
         if (e.keyCode == 13) {
             changeTicker(); 
+            $(this).blur(); 
         }
     });
 
